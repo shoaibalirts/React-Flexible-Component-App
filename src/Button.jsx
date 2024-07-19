be72aad6486a2ec7c3f7c1d908e835c57a156dc3
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-19 12:19:04 +0200

    Assignment is completed

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,36 +1,28 @@
-export default function Button({ children, ...props }) {
-  // Todo: Build this component!
-  // !!! Important:
-  // Wrap the icon with a <span className="button-icon"> to achieve the target look
-  // Also wrap the children prop with a <span>
-  // console.log(children);
-  // console.log(props.Icon);
+export default function Button({
+  children,
+  className,
+  mode = "filled",
+  Icon,
+  ...props
+}) {
+  let cssClasses = `button ${mode}-button`;
 
-  if (props.mode === "filled") {
-    return <button className="filled-button">{children}</button>;
-  } else if (props.mode === "outline") {
-    return <button className="outline-button">{children}</button>;
-  } else if (props.mode === "text") {
-    return <button className="text-button">{children}</button>;
-  } else if (props.mode === "filled" && disabled) {
-    return (
-      <button className="filled-button" disabled={props.disabled}>
-        {children}
-      </button>
-    );
-  } else if (props) return <button {...props}>{children}</button>;
-  // else if (props.Icon) {
-  //   console.log(props.Icon);
-  //   return;
-  // (
-  //   <button>
-  //     <span className="button-icon">
-  //       <Icon>
-  //         <span>{children}</span>
-  //       </Icon>
-  //     </span>
-  //   </button>
-  // );
+  if (Icon) {
+    cssClasses += " icon-button";
+  }
 
-  return <button>{children}</button>;
+  if (className) {
+    cssClasses += " " + className;
+  }
+
+  return (
+    <button className={cssClasses} {...props}>
+      {Icon && (
+        <span className="button-icon">
+          <Icon />
+        </span>
+      )}
+      <span>{children}</span>
+    </button>
+  );
 }
